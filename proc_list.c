b3d7a746d2b4d4839b467f08eb018e547e059ad0
ereizas <ereizas@gmail.com>
2024-10-12 17:46:42 -0400

    Added documentation

--- proc_list.c
+++ proc_list.c
@@ -1,6 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "proc_list.h"
+/*
+Adds a process id to the process list, reallocating when necessary
+@param proc_list process list to add to
+@param new_pid pid for the new process
+*/
 void add(Proc_List *proc_list, pid_t new_pid){
     if(proc_list->num_procs==proc_list->proc_limit){
         if((realloc(proc_list->pids,(proc_list->proc_limit)*2*sizeof(pid_t)))==NULL){
