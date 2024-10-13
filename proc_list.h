688c40797784c4c9d0bad801b0e14874948b3ed1
ereizas <ereizas@gmail.com>
2024-10-12 20:09:22 -0400

    Separated typedef alias and struct declaration

--- proc_list.h
+++ proc_list.h
@@ -1,16 +1,17 @@
 #include "sys/types.h"
-
+#include <stdio.h>
 /*
 @param pids list of ids of processes started by the shell
 @param num_procs number of processes started by the shell
 @param proc_limit current limit for number of processes that can be tracked
 */
-typedef struct{
+struct  Process_List{
     pid_t *pids;
     unsigned int num_procs;
     unsigned int proc_limit;
-    void (*add)(struct Proc_List, pid_t new_pid);
-} Proc_List;
+    void (*add)(struct Process_List *proc_list, pid_t new_pid);
+};
+typedef struct Process_List Proc_List;
 
 void add(Proc_List *proc_list, pid_t new_pid);
 Proc_List create_proc_list();
