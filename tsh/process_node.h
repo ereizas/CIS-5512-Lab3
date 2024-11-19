f14940bc95699d0d92e89ea8b4ccdfaa5012cbe2
ereizas <ereizas@gmail.com>
2024-11-19 12:18:29 -0500

    Included reference to a node as param and returned the new head for add method

--- tsh/process_node.h
+++ tsh/process_node.h
@@ -1,9 +1,10 @@
 #include <stdlib.h>
+#include "synergy.h"
 typedef struct ProcessNode{
    pid_t pid;
    char cmd[CMD_MAX];
    struct ProcessNode *next;
 } ProcessNode;
 
-void add_bg_proc(pid_t pid, const char *cmd);
-void rm_bg_proc(pid_t pid);
+ProcessNode* add_bg_proc(ProcessNode *node, pid_t pid, const char *cmd);
+void rm_bg_proc(ProcessNode *node, pid_t pid);
