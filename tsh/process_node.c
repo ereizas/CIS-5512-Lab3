a0cb9720477c366da5c9fb531a17a862f73eb135
ereizas <ereizas@gmail.com>
2024-11-20 19:10:17 -0500

    Fixed return on error value

--- tsh/process_node.c
+++ tsh/process_node.c
@@ -5,13 +5,13 @@ Adds a node to be the new head of the linked list
 @param node : the current head of the list
 @param pid : process id of the new process to add
 @param cmd : cmd of the new process to add
-@return new_node : the node added which is now the head of the list
+@return new_node : the node added which is now the head of the list or NULL on error
 */
 ProcessNode* add_bg_proc(ProcessNode *node, pid_t pid, const char *cmd){
     ProcessNode *new_node;
     if((new_node=malloc(sizeof(ProcessNode)))==NULL){
         perror("malloc");
-        return;
+        return NULL;
     }
     new_node->pid = pid;
     strcpy(new_node->cmd,cmd);
